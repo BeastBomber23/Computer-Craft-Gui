c98df599a67fc30cb037a0232f4ab5d0aaf3f0c1
BeastBomber23 <59712082+BeastBomber23@users.noreply.github.com>
2021-05-27 13:12:29 -0400

    Toggle

--- gui.lua
+++ gui.lua
@@ -4,12 +4,17 @@ mon.clear()
 
 guiID = 1
 
+--Settings
+scaleTextWithTextSize = false
 
 --Button Saves
 buttonsCallback = {}
 buttonsG = {}
 buttonsL = {}
 buttonsY = {}
+buttonData = {} --Only works for toggles
+buttonType = {} --Normal, Toggle
+buttons = {}
 
 function screenReset()
 
@@ -59,9 +64,7 @@ local function find_index(tab, val)
             return index
         end
     end
-
     return -1
-
 end
 
 function screenCheck()
@@ -69,16 +72,36 @@ function screenCheck()
     local event, side, x, y = os.pullEvent()
 
     if event == "monitor_touch" then
-        print(x .. " " .. y .. "   " .. tostring(has_value(buttonsL, x, 1)) .. "   " .. tostring(has_value(buttonsG, x, 2)) .. "   " .. tostring(has_value(buttonsY, y, 0)))
         x1 = has_value(buttonsL, x, 1)
         x2 = has_value(buttonsG, x, 2)
         l = has_value(buttonsY, y, 0)
         if x1 == true and x2 == true and l == true then
-            callback = buttonsCallback[find_index(buttonsY, y)]
-            callback()
+            type = buttonType[find_index(buttonsY, y)]
+
+            if type == "Normal" then
+                callback = buttonsCallback[find_index(buttonsY, y)]
+                callback()
+            elseif type == "Toggle" then
+
+                data = buttonData[find_index(buttonsY, y)]
+
+                if data == false then
+                    buttonData[find_index(buttonsY, y)] = true  
+                    callback = buttonsCallback[find_index(buttonsY, y)]
+                    buttons[find_index(buttonsY, y)]:changeBox(true)
+                    callback(true)
+                elseif data == true then
+                    buttonData[find_index(buttonsY, y)] = false
+                    callback = buttonsCallback[find_index(buttonsY, y)]
+                    buttons[find_index(buttonsY, y)]:changeBox(false)
+                    callback(false)
+                end
+            end
         end
     end
 end
+
+--Gui Classes
 
 local button = {
 
@@ -103,7 +126,55 @@ local button = {
         table.insert(buttonsG, string.len(text) + (w-string.len(text))/2+1)
         table.insert(buttonsL, (w-string.len(text))/2)
         table.insert(buttonsY, guiID - 1)
+        table.insert(buttonType, "Normal")
+        table.insert(buttonData, false)
         
+    end
+
+}
+
+local toggle = {
+
+    boxPos = 0,
+    id = 0,
+
+    new = function(self)
+        local new = {}
+        setmetatable(new, {__index = self})
+        return new
+    end,
+    create = function(self, text, callback)
+
+        mon.setBackgroundColor(colors.red)
+        w, h = mon.getSize()
+        mon.setCursorPos((w-string.len(text))/2+1, guiID)
+        mon.write("[ ]")
+        mon.setTextColor(colors.white)
+        mon.setCursorPos((w-string.len(text))/2+4, guiID)
+        mon.write(text)
+        
+        boxPos = (w-string.len(text))/2+1
+        id = guiID
+
+        guiID = guiID + 1
+
+        table.insert(buttonsCallback, callback)
+        table.insert(buttonsG, string.len(text) + (w-string.len(text))/2+1)
+        table.insert(buttonsL, (w-string.len(text))/2)
+        table.insert(buttonsY, guiID - 1)
+        table.insert(buttonType, "Toggle")
+        table.insert(buttonData, false)
+        
+    end,
+    changeBox = function(self, toggle)
+
+        mon.setCursorPos(boxPos, id)
+        if toggle == true then
+            mon.write("[X]")
+        else
+            mon.write("[ ]")
+        end
+
     end
 
 }
@@ -112,6 +183,7 @@ local button = {
 
 function addLabel(text, times)
     w, h = mon.getSize()
+
     mon.setCursorPos((w-string.len(text) * times)/2+1, guiID)
 
     text2 = ""
@@ -126,7 +198,13 @@ end
 
 function settings(fontSize)
 
-    mon.setTextScale(fontSize)
+    if fontSize == -1 then
+        scaleTextWithTextSize = true
+    elseif fontSize == -2 then
+        scaleTextWithTextSize = false
+    else
+        mon.setTextScale(fontSize)
+    end
 
 end
 
@@ -138,5 +216,14 @@ function addButton(text, callback)
 
     newButton = button:new()
     newButton:create(text, callback)
+    table.insert(buttons, newButton)
+
+end
+
+function addToggle(text, callback)
+
+    newToggle = toggle:new()
+    newToggle:create(text, callback)
+    table.insert(buttons, newToggle)
 
 end
