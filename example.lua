df119c64d077928d928b4630c80d157b2f5f3eea
BeastBomber23 <59712082+BeastBomber23@users.noreply.github.com>
2021-05-27 13:12:57 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,17 +1,27 @@
-os.loadAPI("gui.lua")
+os.loadAPI("/files/gui.lua")
 
 lastSelectedPage = -1
 currentPage = 0
 
 function backP()
 
-	currentPage = lastSelectedPage - 1
+    currentPage = lastSelectedPage - 1
 
 end
 
 function nextP()
 
-	currentPage = lastSelectedPage + 1
+    currentPage = lastSelectedPage + 1
+
+end
+
+function toggle(onOff)
+
+    if onOff == true then
+        print("Toggled On")
+    else
+        print("Toggled Off")
+    end
 
 end
 
@@ -20,34 +30,35 @@ while true do
 
     if currentPage == 0 then
 
-    	gui.screenReset()
-    	gui.addLabel("Page 1|3", 1)
-    	gui.addSpacer()
-    	gui.addButton("Next", nextP)
+        gui.screenReset()
+        gui.addLabel("Page 1|3", 1)
+        gui.addSpacer()
+        gui.addToggle("Test Toggle", toggle)
+        gui.addButton("Next", nextP)
 
-    	lastSelectedPage = 0
-    	currentPage = -1
+        lastSelectedPage = 0
+        currentPage = -1
 
     elseif currentPage == 1 then
 
-    	gui.screenReset()
-    	gui.addLabel("Page 2|3", 1)
-    	gui.addSpacer()
-    	gui.addButton("Next", nextP)
-    	gui.addButton("Back", backP)
+        gui.screenReset()
+        gui.addLabel("Page 2|3", 1)
+        gui.addSpacer()
+        gui.addButton("Next", nextP)
+        gui.addButton("Back", backP)
 
-    	lastSelectedPage = 1
-    	currentPage = -1
+        lastSelectedPage = 1
+        currentPage = -1
 
     elseif currentPage == 2 then
 
-    	gui.screenReset()
-    	gui.addLabel("Page 3|3", 1)
-    	gui.addSpacer()
-    	gui.addButton("Back", backP)
+        gui.screenReset()
+        gui.addLabel("Page 3|3", 1)
+        gui.addSpacer()
+        gui.addButton("Back", backP)
 
-    	lastSelectedPage = 2
-    	currentPage = -1
+        lastSelectedPage = 2
+        currentPage = -1
 
     end
 
